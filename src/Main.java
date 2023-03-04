3c27f108423131ed669c159cc37f5e418a3d778c
vadim <vadimpoldolin19@gmail.com>
2023-03-04 20:54:07 +0100

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
+
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+        int[] spendSalary = generateRandomArray();
+        int total = 0;
+        for (int i = 0; i < spendSalary.length; i++){
+            total += spendSalary[i];
+        }
+        System.out.printf("Сумма трат за месяц составила %d рублей",total);
+    }
+    private static void task2() {
+        System.out.println("Задача 2");
 
     }
 }
